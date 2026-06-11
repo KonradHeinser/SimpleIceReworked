10e8697bb37c1b2242b458e4513fcafd9a97dcfe
Alite <128163141+KonradHeinser@users.noreply.github.com>
2026-06-11 11:43:47 -0400

    1.6 Update

---
+++ Source/OnlyPlaceOnIce/PlaceWorker_Ice.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace OnlyPlaceOnIce
+{
+    public class PlaceWorker_Ice : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            if (loc.GetTerrain(map).defName != "Ice")
+                return new AcceptanceReport("OPOI_NeedIce".Translate());
+
+            return base.AllowsPlacing(checkingDef, loc, rot, map, thingToIgnore, thing);
+        }
+    }
+}
